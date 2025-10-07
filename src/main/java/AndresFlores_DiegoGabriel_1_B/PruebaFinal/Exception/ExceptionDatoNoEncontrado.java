5c9bd0a0331040f376bbcae20010ffc80ddc8be4
Andres Flores <20240147@ricaldone.edu.sv>
2025-10-07 11:24:20 -0600

    Arroz3

---
+++ src/main/java/AndresFlores_DiegoGabriel_1_B/PruebaFinal/Exception/ExceptionDatoNoEncontrado.java
@@ -0,0 +1,7 @@
+package AndresFlores_DiegoGabriel_1_B.PruebaFinal.Exception;
+
+public class ExceptionDatoNoEncontrado extends RuntimeException {
+  public ExceptionDatoNoEncontrado(String message) {
+    super(message);
+  }
+}
